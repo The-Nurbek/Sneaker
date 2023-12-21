9455abe849c442e88037849e55679690755bb1f5
ShinJoku21 <snaiper_iz_tashkenta@mail.ru>
2023-12-21 15:11:35 +0500

    first commit

--- src/components/modal/ModalPurchased.vue
+++ src/components/modal/ModalPurchased.vue
@@ -2,11 +2,11 @@
     <div>
         <h2 class="inprocess__title">Корзина</h2>
         <div class="empty">
-            <img src="../assets/img/done.jpg" alt="">
+            <img src="@/assets/img/done.jpg" alt="">
             <h3 class="empty__title green">Заказ оформлен!</h3>
             <p class="empty__text">Ваш заказ #18 скоро будет передан курьерской доставке</p>
             <router-link to="/" @click="Store.modal = false, this.$router.go(0)" class="empty_btn"> <img
-                    src="../assets/img/emback.png" alt="">Вернуться назад</router-link>
+                    src="@/assets/img/emback.png" alt="">Вернуться назад</router-link>
         </div>
     </div>
 </template>
